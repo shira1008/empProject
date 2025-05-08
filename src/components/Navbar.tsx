a7ddb75f5118d51e8b3e62814294dd1d9cda9be6
shira1008 <shaharshira1008@gmail.com>
2025-05-08 12:03:44 +0300

    added navbar for small screens state and css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,23 @@
 import '../Navbar.css';
+import { useState } from 'react';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div className='navbar'>
       <div className='logo'>Employees</div>
-      <div className='nav-links'>
+
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <a href='#'>Home</a>
         <a href='#' className='active'>
           Employees
         </a>
         <a href='#'>About</a>
         <a href='#'>Contact</a>
+      </div>
+
+      <div className='menu-icon' onClick={() => setIsOpen(!isOpen)}>
+        <span className='material-icons'>menu</span>
       </div>
     </div>
   );
